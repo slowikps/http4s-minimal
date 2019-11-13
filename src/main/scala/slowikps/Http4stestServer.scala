450dfbfca34568faf48ecd419d0c59a119444be8
slowikps <slowikps@gmail.com>
2019-11-13 21:13:56 +0000

    Another test

--- src/main/scala/slowikps/Http4stestServer.scala
+++ src/main/scala/slowikps/Http4stestServer.scala
@@ -1,6 +1,7 @@
 package slowikps
 
-import cats.effect.{ConcurrentEffect, ContextShift, IO, Timer}
+import cats.data.Kleisli
+import cats.effect.{ContextShift, IO, Timer}
 import cats.implicits._
 import fs2.Stream
 import org.http4s.client.Client
@@ -8,6 +9,7 @@ import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
+import org.http4s.{Header, HttpRoutes, Request, Status}
 
 import scala.concurrent.ExecutionContext.global
 
@@ -16,20 +18,19 @@ object Http4stestServer {
   def stream(implicit T: Timer[IO], C: ContextShift[IO]): Stream[IO, Nothing] = {
     for {
       client: Client[IO] <- BlazeClientBuilder[IO](global).stream
-      helloWorldAlg = HelloWorld.impl[IO]
-      jokeAlg       = Jokes.impl(client)
+      helloWorldAlg      = HelloWorld.impl[IO]
+      jokeAlg            = Jokes.impl(client)
       // Combine Service Routes into an HttpApp.
       // Can also be done via a Router if you
       // want to extract a segments not checked
       // in the underlying routes.
-      httpApp = (
-        Http4stestRoutes.helloWorldRoutes[IO](helloWorldAlg) <+>
-          Http4stestRoutes.jokeRoutes[IO](jokeAlg) <+>
-          Http4stestRoutes.pawelRoutes
-      ).orNotFound
+      allRoutes: HttpRoutes[IO] = Http4stestRoutes.helloWorldRoutes[IO](helloWorldAlg) <+>
+        Http4stestRoutes.jokeRoutes[IO](jokeAlg) <+>
+        Http4stestRoutes.pawelRoutes
 
+      wrapped = myMiddle(allRoutes, Header("SomeKey", "SomeValue"))
       // With Middlewares in place
-      finalHttpApp = Logger.httpApp(true, true)(httpApp)
+      finalHttpApp = Logger.httpApp(true, true)(wrapped.orNotFound)
 
       exitCode <- BlazeServerBuilder[IO]
                    .bindHttp(8080, "0.0.0.0")
@@ -37,4 +38,13 @@ object Http4stestServer {
                    .serve
     } yield exitCode
   }.drain
+
+  def myMiddle(service: HttpRoutes[IO], header: Header): HttpRoutes[IO] = Kleisli { req: Request[IO] =>
+    service(req).map {
+      case Status.Successful(resp) =>
+        resp.putHeaders(header)
+      case resp =>
+        resp
+    }
+  }
 }
--- src/main/scala/slowikps/Jokes.scala
+++ src/main/scala/slowikps/Jokes.scala
@@ -2,22 +2,21 @@ package slowikps
 import cats.Applicative
 import cats.effect.{IO, Sync, Timer}
 import cats.implicits._
-import io.circe.{Decoder, Encoder, HCursor, Json}
 import io.circe.generic.semiauto._
-import org.http4s._
-import org.http4s.implicits._
-import org.http4s.{EntityDecoder, EntityEncoder, Method, Request, Uri}
-import org.http4s.client.Client
-import org.http4s.client.dsl.Http4sClientDsl
+import io.circe.{Decoder, Encoder, Json}
 import org.http4s.Method.{GET, _}
 import org.http4s.circe._
+import org.http4s.client.Client
+import org.http4s.client.dsl.Http4sClientDsl
+import org.http4s.implicits._
+import org.http4s.{EntityDecoder, EntityEncoder}
 
 import scala.concurrent.duration._
 import scala.util.{Failure, Success, Try}
-trait Jokes[F[_]]{
+trait Jokes[F[_]] {
   def get(implicit timer: Timer[IO]): F[Jokes.Joke]
   def post: F[Jokes.Joke]
-  def getWithParams(map: Map[String, Seq[String]]) : F[Jokes.Joke]
+  def getWithParams(map: Map[String, Seq[String]]): F[Jokes.Joke]
 }
 
 object Jokes {
@@ -37,14 +36,14 @@ object Jokes {
 
   final case class JokeError(e: Throwable) extends RuntimeException
 
-  def impl(C: Client[IO]): Jokes[IO] = new Jokes[IO]{
-    val dsl = new Http4sClientDsl[IO]{}
+  def impl(C: Client[IO]): Jokes[IO] = new Jokes[IO] {
+    val dsl = new Http4sClientDsl[IO] {}
     import dsl._
     def get(implicit timer: Timer[IO]): IO[Jokes.Joke] = {
       for {
         _ <- IO.sleep(1.second)
         res <- C.expect[Joke](GET(uri"https://icanhazdadjoke.com/"))
-          .adaptError{ case t => JokeError(t)} // Prevent Client Json Decoding Failure Leaking
+                .adaptError { case t => JokeError(t) } // Prevent Client Json Decoding Failure Leaking
       } yield res
     }
 
@@ -55,7 +54,7 @@ object Jokes {
           C.expect[Json](POST(json"""{"name": "Alice"}""", uri"https://postman-echo.com/post"))
             .unsafeRunSync()
         } match {
-          case Success(value) => println(value)
+          case Success(value)     => println(value)
           case Failure(exception) => exception.printStackTrace()
         }
         C.expect[Json](POST(json"""{"my_joke": "very bad joke"}""", uri"https://postman-echo.com/post"))
@@ -65,13 +64,11 @@ object Jokes {
       }
     }
 
-    override def getWithParams(map: Map[String, Seq[String]]) : IO[Joke] = {
+    override def getWithParams(map: Map[String, Seq[String]]): IO[Joke] = {
       {
-        import io.circe.syntax._
-        C.expect[Json](
-          GET(map.foldLeft(uri"https://postman-echo.com/get"){
+        C.expect[Json](GET(map.foldLeft(uri"https://postman-echo.com/get") {
             case (acc, (name, value)) => acc.withQueryParam(name, value)
-          } ))
+          }))
           .map(_.hcursor.downField("args").as[Json].getOrElse("ERROR"))
           .map(json => Joke(json.toString))
 
